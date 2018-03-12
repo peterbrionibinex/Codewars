c12811af3ccb5bf982c15fabcdfe794467835bd8
peter.br <peter.br@ibinex.tech>
2018-03-12 03:37:22 -0400

    First program solved at Codewars

---
+++ PHP/Multiply.php
@@ -0,0 +1,18 @@
+<?php
+
+  /* 
+    First program solved at Codewars
+    at the same time, it is the first
+    problem that you need to solve
+    for you to successfully 
+    registered at Codewars
+    
+    Program Name: Multiply.php
+    
+  */
+
+  function multiply($a, $b){
+    return $a*$b;
+  }
+
+multiply(4,3);
